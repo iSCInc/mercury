60488338c9a573b8542ff54a8a2d67f549aa503f
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-12-29 15:59:46 +0100

    test 10, brake test

--- test/specs/public/scripts/main/components/AdSlotComponent.js
+++ test/specs/public/scripts/main/components/AdSlotComponent.js
@@ -8,7 +8,7 @@ test('Name lower case', function () {
 	var component = this.subject(),
 		testCases = [
 			{
-				name: 'test',
+				name: 'test1',
 				expected: 'test',
 				description: 'No change'
 			} , {
