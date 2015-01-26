3714b9636020bbe7674245740329c5e088c4786f
dianafa <dianafa@o2.pl>
2015-01-26 12:43:37 +0100

    not important small changes

--- public/scripts/main/controllers/ArticleController.ts
+++ public/scripts/main/controllers/ArticleController.ts
@@ -11,7 +11,7 @@ App.ArticleController = Em.ObjectController.extend({
 	commentsPage: null,
 	map: null,
 	noAds: Em.computed.alias('controllers.application.noAds'),
-	uselang: Em.computed.alias('controllers.application.useLang'),
+	useLang: Em.computed.alias('controllers.application.useLang'),
 
 	actions: {
 		updateHeaders: function (headers: NodeList): void {
