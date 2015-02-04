d97b915fca9105dc9c4c5c55b5d3f8d523e0562d
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-02-04 23:07:33 +0100

    fix tests

--- test/specs/front/scripts/main/mixins/AdsMixin.js
+++ test/specs/front/scripts/main/mixins/AdsMixin.js
@@ -1,23 +1,39 @@
-module("AdsMixin");
+'use strict';
 
-test('setup ads context', function() {
-	var context = {
-			a: 1
-		},
-		gotContext = false,
-		runCalled = false,
-		testObj = Em.Object.createWithMixins(App.AdsMixin, {}),
-		original = Mercury.Modules.Ads.getInstance;
+QUnit.module('AdsMixin');
 
-	Mercury.Modules.Ads.getInstance = function () {
-		return {
-			reload: function(context) {
-				gotContext = context;
+QUnit.test('has its data', function () {
+	this.stub(Mercury.Modules.Ads, 'getInstance', function (){});
+
+	var instance = Em.Object.createWithMixins(App.AdsMixin, {});
+
+	ok(instance.adsData);
+});
+
+QUnit.test('appendAd', function () {
+	var instance = Em.Object.createWithMixins(App.AdsMixin, {}),
+		elementStub = this.stub({
+			before: function () {}
+		}),
+		triggerSpy = this.spy(),
+		createElementSpy = this.spy(function () {
+			return {
+				$: function () {
+					return ''
+				},
+				trigger: triggerSpy
 			}
-		}
-	};
+		});
 
-	testObj.setupAdsContext(context);
-	Mercury.Modules.Ads.getInstance = original;
-	equal(gotContext, context, 'Set the ads context');
+	instance.createChildView = this.spy(function () {
+		return {
+			createElement: createElementSpy
+		}
+	});
+
+	instance.appendAd('test', 'before', elementStub);
+
+	ok(instance.createChildView.called);
+	ok(createElementSpy.called);
+	ok(triggerSpy.calledWith('didInsertElement'), 'called with didInsertElement');
 });
--- test/specs/front/scripts/mercury/modules/Ads.js
+++ test/specs/front/scripts/mercury/modules/Ads.js
@@ -9,60 +9,57 @@ QUnit.test('Init method works', function() {
 	var calledURL,
 		callbackIsCalled = false,
 		testUrl = 'http://example.com/',
-		instance = Mercury.Modules.Ads.getInstance(),
-		loadStub = this.stub(M, 'load', function(url, callback) {
-			calledURL = url;
-			callback();
-		});
-	require = function(modules, callback){
-		callback();
+		instance = new Mercury.Modules.Ads(),
+		runSpy = this.spy(),
+		requiredModules;
+
+	require = function(modules, callback) {
+		requiredModules = modules;
+		callback({run: runSpy}, modules[1], modules[2]);
 	};
+
+	this.stub(M, 'load', function(url, callback) {
+		calledURL = url;
+		callback();
+	});
 
 	instance.init(testUrl, function () {
 		callbackIsCalled = true;
 	});
+
 	delete(require);
+
+	ok(runSpy.calledWithExactly(requiredModules[2], [], 'queue.mercury'), 'run called with correct args');
 	equal(calledURL, testUrl);
 	equal(callbackIsCalled, true);
 });
 
-QUnit.test('Reload ads works', function () {
+QUnit.test('setContext ads works', function () {
 	var adEngineRun = false,
 		testContext = {
 			test: 1
 		},
 		setContextSpy = this.spy(),
-		runSpy = this.spy(),
-		instance = Mercury.Modules.Ads.getInstance();
+		instance = new Mercury.Modules.Ads();
 
 	instance.adContextModule = {
 		setContext: setContextSpy
 	};
-	instance.adEngineModule = {
-		run: runSpy
-	};
-	instance.adConfigMobile = {
-		test: 2
-	};
-	instance.adSlots = [
-		['slot1']
-	];
 
-	instance.reload(testContext);
+	instance.isLoaded = true;
+
+	instance.setContext(testContext);
+
 	ok(setContextSpy.calledWith(testContext));
-	ok(runSpy.calledWith(instance.adConfigMobile, instance.adSlots, 'queue.mercury'));
-	instance.adContextModule = undefined;
-	instance.adEngineModule = undefined;
-	instance.adConfigMobile = undefined;
-	instance.adSlots = [];
+	expect(instance.adsContext, testContext);
 });
 
 QUnit.test('Add/remove slots works', function () {
-	var instance = Mercury.Modules.Ads.getInstance();
+	var instance = new Mercury.Modules.Ads();
+
 	equal(instance.adSlots.length, 0);
 	instance.addSlot('test1');
 	equal(instance.adSlots.length, 1);
-	instance.removeSlot('test1');
-	equal(instance.adSlots.length, 0);
-	instance.reload(null);
+
+	instance.setContext(null);
 });
