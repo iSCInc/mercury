1dee863cd58a8f52743266c74341049aa213ab3b
Igor Rogatty <igor@wikia-inc.com>
2015-02-23 10:45:29 +0100

    Check if it's Fastclick's fault

--- front/scripts/mercury/init.ts
+++ front/scripts/mercury/init.ts
@@ -3,5 +3,5 @@ declare var FastClick: any;
 $(function() {
 	// FastClick disables the 300ms delay on iOS and some Android devices. It also uses clicks so that
 	// elements have access to :hover state
-	FastClick.attach(document.body);
+	//FastClick.attach(document.body);
 });
